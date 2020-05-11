4bd7449db3f89a1174e3abda9bfe7a6520f102b4
Yuki Esaki <yesaki610@gmail.com>
2020-05-12 02:47:02 +0900

    スクールの教科書を見てカウントアプリを作り直しました

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plas() {
+        number = number + 1
+        label.text = String(number)
     }
 
 
